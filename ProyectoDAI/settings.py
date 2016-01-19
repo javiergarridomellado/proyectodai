5d804ba2c809f87260cb83a6c73ef8984b584632
javiergarridomellado <javiergarridomellado@correo.ugr.es>
2016-01-19 23:53:47 +0100

    settt

--- ProyectoDAI/settings.py
+++ ProyectoDAI/settings.py
@@ -93,7 +93,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'postgres',
-        'USER': 'postgres',
+        'USER': 'vagrant',
         'HOST': '',
         'PORT': '',
     }
