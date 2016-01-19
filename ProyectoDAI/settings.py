af36a508a47ab34ab61928e738a122ed8699553d
javiergarridomellado <javiergarridomellado@correo.ugr.es>
2016-01-19 22:01:30 +0100

    borrar

--- ProyectoDAI/settings.py
+++ ProyectoDAI/settings.py
@@ -83,10 +83,20 @@ TEMPLATE_DIRS = (
 # Database
 # https://docs.djangoproject.com/en/1.7/ref/settings/#databases
 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#    }
+#}
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'vagrant',
+        'USER': 'vagrant',
+        'PASSWORD': 'vagrant',
+        'HOST': '',
+        'PORT': '',
     }
 }
 #ON_HEROKU = os.environ.get('PORT')
