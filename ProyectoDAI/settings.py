772786f048bf5678b16cac698e294fe146f768ac
javiergarridomellado <javiergarridomellado@correo.ugr.es>
2016-02-03 11:46:28 +0100

    heroku

--- ProyectoDAI/settings.py
+++ ProyectoDAI/settings.py
@@ -77,12 +77,26 @@ WSGI_APPLICATION = 'ProyectoDAI.wsgi.application'
 
 #TEMPLATE_DIRS = ('ProyectoDAI/restaurante/',)
 TEMPLATE_PATH = os.path.join(BASE_DIR, 'templates')
-
-TEMPLATE_DIRS = (
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [TEMPLATE_PATH],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+#TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    TEMPLATE_PATH,
+    #TEMPLATE_PATH,
 )
 # Database
 # https://docs.djangoproject.com/en/1.7/ref/settings/#databases
