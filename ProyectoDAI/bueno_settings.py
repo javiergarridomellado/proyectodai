9722f9044443c68eb4c1285e0e1ad2311815c781
javiergarridomellado <javiergarridomellado@correo.ugr.es>
2016-02-03 20:34:55 +0100

    css

--- ProyectoDAI/bueno_settings.py
+++ ProyectoDAI/bueno_settings.py
@@ -1,17 +1,19 @@
 """
-Django settings for TusPachangas project.
+Probando despliegue heroku
+Cambios
+Django settings for ProyectoDAI project.
 For more information on this file, see
 https://docs.djangoproject.com/en/1.7/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/1.7/ref/settings/
 """
-import django
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
+import dj_database_url
+import django
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
-TEMPLATE_PATH = os.path.join(BASE_DIR, 'templates')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.7/howto/deployment/checklist/
@@ -24,8 +26,12 @@ DEBUG = True
 
 TEMPLATE_DEBUG = True
 
+#ALLOWED_HOSTS = ['*']
 ALLOWED_HOSTS = []
 
+
+#Restricting Access with a Decorator
+LOGIN_URL = '/restaurante/login/'
 
 # Application definition
 
@@ -36,17 +42,20 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'registration',	#add in the registration package
 	'rest_framework',
 	'restaurante',
 	'easy_maps',
 )
 
+
+
 if django.VERSION < (1, 7):
     INSTALLED_APPS += (
         'south',
     )
-    
+
+EASY_MAPS_CENTER = (-41.3, 32)
+
 MIDDLEWARE_CLASSES = (
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -62,8 +71,11 @@ ROOT_URLCONF = 'ProyectoDAI.urls'
 WSGI_APPLICATION = 'ProyectoDAI.wsgi.application'
 
 
-TEMPLATE_DIRS = (TEMPLATE_PATH,)
 
+#TEMPLATE_DIRS = ('ProyectoDAI/restaurante/',)
+TEMPLATE_PATH = os.path.join(BASE_DIR, 'templates')
+
+TEMPLATE_DIRS = ( TEMPLATE_PATH,)
 # Database
 # https://docs.djangoproject.com/en/1.7/ref/settings/#databases
 
@@ -73,7 +85,15 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+ON_HEROKU = os.environ.get('PORT')
+if ON_HEROKU:
+	DATABASE_URL='postgres://kytzveedsclzaf:eIJAAuElYvSxPK-vmSdXG9Hjv8@ec2-107-21-219-235.compute-1.amazonaws.com:5432/df9sfr7a9b8vjf'
+	DATABASES = {'default': dj_database_url.config(default=DATABASE_URL)}
 
+
+
+
+#STATIC_ROOT = 'staticfiles'
 # Internationalization
 # https://docs.djangoproject.com/en/1.7/topics/i18n/
 
@@ -90,16 +110,17 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
+
+#STATIC_URL = '/static/'
 STATIC_PATH = os.path.join(BASE_DIR,'static')
 
-STATIC_URL = '/static/'
+STATIC_URL = '/static/' # You may find this is already defined as such.
 
 STATICFILES_DIRS = (
     STATIC_PATH,
 )
 
-#Media
+#STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-
-
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media') # Absolute path to the media directory
