f2cd08bb0669a4f888cee1e803f914825ae5cbec
javiergarridomellado <javiergarridomellado@correo.ugr.es>
2016-02-03 20:03:46 +0100

    css

--- ProyectoDAI/urls.py
+++ ProyectoDAI/urls.py
@@ -1,5 +1,7 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from django.conf import settings # New Import
+from django.conf.urls.static import static # New Import
 
 
 
@@ -12,6 +14,21 @@ urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
 	url(r'^restaurante/', include('restaurante.urls')),
 )
+
+if not settings.DEBUG:
+	urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+urlpatterns += staticfiles_urlpatterns()
+urlpatterns += patterns('',
+        (r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
+    )
+
+if settings.DEBUG:
+	urlpatterns += patterns(
+		'django.views.static',
+		(r'^media/(?P<path>.*)',
+		'serve',
+		{'document_root': settings.MEDIA_ROOT}), )
 '''
 from django.conf import settings # New Import
 from django.conf.urls.static import static # New Import
