6a047759029a5c54b14f3d17362e15d8b071ba26
javiergarridomellado <javiergarridomellado@correo.ugr.es>
2016-02-03 12:11:48 +0100

    heroku

--- ProyectoDAI/settings.py
+++ ProyectoDAI/settings.py
@@ -118,7 +118,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
-STATIC_ROOT = 'static'
+STATIC_ROOT = 'staticfiles'
 #STATIC_URL = '/static/'
 STATIC_PATH = os.path.join(BASE_DIR,'static')
 
