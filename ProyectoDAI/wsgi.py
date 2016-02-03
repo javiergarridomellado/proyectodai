47ad66cc0f69a250e16cceebb707ec2904f6f443
javiergarridomellado <javiergarridomellado@correo.ugr.es>
2016-02-03 10:58:36 +0100

    heroku prueba

--- ProyectoDAI/wsgi.py
+++ ProyectoDAI/wsgi.py
@@ -9,16 +9,17 @@ https://docs.djangoproject.com/en/1.7/howto/deployment/wsgi/
 
 import os
 from django.core.wsgi import get_wsgi_application
-from dj_static import Cling
-
+#from dj_static import Cling
+from whitenoise.django import DjangoWhiteNoise
 
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ProyectoDAI.settings")
 
 
 application = get_wsgi_application()
-application = Cling(get_wsgi_application())
+#application = Cling(get_wsgi_application())
 
+application = DjangoWhiteNoise(application)
 
 
 
